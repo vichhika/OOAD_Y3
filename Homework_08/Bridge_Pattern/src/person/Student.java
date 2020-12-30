53c12ee79a540ffef7f83678073dc8048dc6f6f1
Sambath <dilytithsambath@gmail.com>
2020-12-30 10:12:36 +0700

    Add submit history to Student class

--- Homework_08/Bridge_Pattern/src/person/Student.java
+++ Homework_08/Bridge_Pattern/src/person/Student.java
@@ -1,11 +1,17 @@
 package person;
 
+import java.util.ArrayList;
+
 public class Student extends Person{
+    private ArrayList<String> submitHistory = new ArrayList<>();
     @Override
     public void takeAction(String action){
         switch(action){
             case "ViewHW" : /*View Homework*/ break;
             case "Submit" : /*Submit Homework*/ break;
         }
+    }
+    public ArrayList getSubmitHistory(){
+        return submitHistory;
     }
 }
